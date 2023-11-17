3bd19c3018713a135eb5eeda4887321c1f6b8b36
Igor Stepura <igorgames13@mail.ru>
2023-11-17 19:36:01 +0600

    finished version + responsive menu

--- index.js
+++ index.js
@@ -1,30 +1,70 @@
-$('.project-examples').slick({
-    infinite: true,
-    speed: 700,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 3000,
-    dots: true,
+function init() {
+    $('.project-examples').slick({
+        infinite: true,
+        speed: 700,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // autoplay: true,
+        // autoplaySpeed: 3000,
+        dots: true,
 
-});
+    });
 
-$('.project-examples').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image',
-    // other options
-    gallery:{
-        enabled:true
+    $('.project-examples').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+        gallery:{
+            enabled:true
+        }
+    });
+
+    let burger = document.getElementById('burger');
+    let close = document.getElementById('close');
+    let menu = document.getElementById('menu');
+    let menuTeam = document.getElementById('menu-team');
+    let menuProjects = document.getElementById('menu-projects');
+    let menuContacts = document.getElementById('menu-contacts');
+    let logo = document.getElementById('logo');
+
+    menuTeam.onclick = () => {
+        close.style.display = "none"
+        burger.style.display = "block"
+        menu.classList.remove('animate__fadeInRight')
+        menu.classList.add('animate__fadeOutRight')
     }
-});
+    menuProjects.onclick = () => {
+        close.style.display = "none"
+        burger.style.display = "block"
+        menu.classList.remove('animate__fadeInRight')
+        menu.classList.add('animate__fadeOutRight')
+    }
+    menuContacts.onclick = () => {
+        close.style.display = "none"
+        burger.style.display = "block"
+        menu.classList.remove('animate__fadeInRight')
+        menu.classList.add('animate__fadeOutRight')
+    }
+    logo.onclick = () => {
+        close.style.display = "none"
+        burger.style.display = "block"
+        menu.classList.remove('animate__fadeInRight')
+        menu.classList.add('animate__fadeOutRight')
+    }
 
+    burger.onclick = () => {
+        burger.style.display = "none"
+        close.style.display = "block"
+        menu.style.display = "block"
+        menu.classList.remove('animate__fadeOutRight')
+        menu.classList.add('animate__fadeInRight')
+    }
 
-document.getElementById('burger').onclick = () => {
-    document.getElementById('burger').style.display = "none"
-    document.getElementById('close').style.display = "block"
+    close.onclick = () => {
+        close.style.display = "none"
+        burger.style.display = "block"
+        menu.classList.remove('animate__fadeInRight')
+        menu.classList.add('animate__fadeOutRight')
+    }
 }
-
-document.getElementById('close').onclick = () => {
-    document.getElementById('close').style.display = "none"
-    document.getElementById('burger').style.display = "block"
-}
+init();
